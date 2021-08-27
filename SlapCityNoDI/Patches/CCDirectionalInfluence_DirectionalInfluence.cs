81d4b441783d3335c4537354e0098a6a7ac1187a
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2021-08-26 20:26:52 -0700

    Add project files.

---
+++ SlapCityNoDI/Patches/CCDirectionalInfluence_DirectionalInfluence.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using Smash;
+
+namespace SlapCityNoDI.Patches
+{
+    [HarmonyPatch(typeof(CCDirectionalInfluence), "DirectionalInfluence")]
+    class CCDirectionalInfluence_DirectionalInfluence
+    {
+        static bool Prefix() => false;
+    }
+}
---
+++ SlapCityNoDI/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace SlapCityNoDI
+{
+    [BepInPlugin("com.steven.slapcity.noDI", "No DI", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            var harmony = new Harmony("com.steven.slapcity.noDI");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
